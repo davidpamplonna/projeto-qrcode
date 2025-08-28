20b64ae5572075c8ffdc9ab7f60d64f91b9158dc
David Pamplona <davypamp@gmail.com>
2025-08-28 07:48:33 -0300

    Adicionando logica de escolha

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import prompt from 'prompt';
+import mainPrompt from './prompts/prompt-main.js';
+
+
+
+async function main() {
+    prompt.get(mainPrompt, async (err, choose) => {
+        if (choose.select == 1) {
+            console.log("Escolheu o qrCODE")
+        }
+
+        else if (choose.select == 2){
+             console.log("Escolheu o password")
+        }
+    })
+
+    prompt.start();
+
+}
+
+
+main();
